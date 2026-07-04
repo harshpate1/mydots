24db3fc1bdb849ea9a63c315ba3fad2ae8df7553
Harsh <harshpa7el@proton.me>
2026-07-04 16:57:51 +0530

    updated wallust template for gtk.css

--- hypr/.config/hypr/colors.lua
+++ hypr/.config/hypr/colors.lua
@@ -1,16 +1,16 @@
 hl.config({
     general = {
         col = {
-            active_border   = "rgba(6F8A4FFF)",
-            inactive_border = "rgba(0D120780)",
+            active_border   = "rgba(758A9CFF)",
+            inactive_border = "rgba(19212880)",
         },
     },
     misc = {
-        background_color = "rgba(0D1207FF)",
+        background_color = "rgba(192128FF)",
     },
 })
 
 hl.window_rule({
     match        = { pin = 1 },
-    border_color = "rgba(B6AEA6AA) rgba(B6AEA677)",
+    border_color = "rgba(B2ACB4AA) rgba(B2ACB477)",
 })
